a6b2176f1a20c3c73df32456b839806344d42334
Fkabwe830-code <fkabwe830@gmail.com>
2025-10-02 00:11:31 +0200

    day2: Hello World printed five times

---
+++ project2.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+int main() {
+    int i;
+    // Loop from 0 to 4 (5 times)
+    for (i = 0; i < 5; i++) {
+        printf("Hello, World!\n");  // Print message each time
+    }
+    return 0;  // Program finished successfully
+}
