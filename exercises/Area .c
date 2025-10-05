700a29a1eaf104b35667740ea14b75ff6a668e99
fkabwe830-code <fkabwe830@gmail.com>
2025-10-05 21:50:12 +0200

    exercise on basic level

---
+++ exercises/Area .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int b;
+    int h;
+    printf("enter the base:\n");
+    scanf("%d",&b);
+    printf("enter the height:\n");
+    scanf("%d",&h);
+    printf("Area is = %d",b*h/2);
+    return 0;
+}
